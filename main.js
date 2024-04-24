04c163be93cea2b26e2a1665a9687e6bdfcb8780
ann <ocirovaanna5@gmail.com>
2024-04-25 01:15:41 +0800

    Проверка encrypt()

---
+++ main.js
@@ -0,0 +1,23 @@
+const encrypt = (text) => {
+    let i = 0;
+    let nextSymbol = '';
+    let result = '';
+    let para = '';
+    for (i = 0; i < text.length; i += 2) {
+      nextSymbol = text[i + 1] || '';
+      para = nextSymbol + text[i];
+      result = `${result}${para}`;
+    }
+    console.log(result);
+    return result;
+    
+  };
+
+
+encrypt('move');   // 'omev'
+encrypt('attack'); // 'taatkc'
+encrypt('car!'); // 'ac!r'
+
+// Если длина строки нечётная,
+// то последний символ остаётся на своём месте
+encrypt('go!'); // 'og!'
